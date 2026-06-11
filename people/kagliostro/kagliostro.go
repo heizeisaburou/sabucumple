a2554216cf49e43f46cc68e9f716eaf80606fe77
ginobadhouse <ginobadhouse@users.noreply.github.com>
2026-06-11 10:07:46 -0300

    feat(kagliostro): mover assets y template a carpeta kagliostro

--- people/kagliostro/kagliostro.go
+++ people/kagliostro/kagliostro.go
@@ -1,7 +1,8 @@
 package kagliostro
 
 import (
-	"net/http"
+	"html/template"
+	"io"
 
 	"github.com/labstack/echo/v5"
 )
@@ -12,17 +13,28 @@ func New() Module {
 	return Module{}
 }
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(c *echo.Context, w io.Writer, name string, data any) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+var t = Template{
+	templates: template.Must(template.ParseGlob("people/kagliostro/assets/*.gohtml")),
+}
+
 func (Module) Endpoint() string {
 	return "kagliostro"
 }
 
 func (Module) Register(g *echo.Group) {
 	g.GET("/", home)
+	g.GET("", home)
+	g.Static("/assets", "people/kagliostro/assets")
 }
 
 func home(c *echo.Context) error {
-	return c.HTML(http.StatusOK, `
-		<h1>kagli</h1>
-		<p>Aquí ejemplo puede hacer lo que quiera.</p>
-		`)
+	return t.Render(c, c.Response(), "home.gohtml", nil)
 }
